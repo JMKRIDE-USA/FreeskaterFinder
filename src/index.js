3c732681ba9dea23ad8140183fc36300a55dccf3
Jeffdude <jeffmilling@gmail.com>
2022-01-05 16:35:12 +0000

    begin using JFH

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { JFHApp } from '@jeffdude/frontend-helpers';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <JFHApp config={{backendURL: "http://192.168.0.88:3600"}}>
+      <App />
+    </JFHApp>
   </React.StrictMode>,
   document.getElementById('root')
 );
