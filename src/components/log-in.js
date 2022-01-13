bf714a4197c84cc5f6e82389510127fc2cf4c357
Jeffdude <jeffmilling@gmail.com>
2022-01-13 00:47:12 +0000

    more login/createaccount stuff

--- src/components/log-in.js
+++ src/components/log-in.js
@@ -1,38 +1,23 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { Button, Dialog, DialogContent, DialogContentText, DialogTitle, Grid, TextField } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { useLogin } from '@jeffdude/frontend-helpers';
 
+import useMakeLoadingButton from './loading-button';
+
 export function SignInDialog({open}) {
   const { register, handleSubmit, formState: {errors}}  = useForm();
-  const [submitted, setSubmitted] = useState(false);
-  const [submissionResult, setSubmissionResult] = useState(undefined);
   const login = useLogin();
-  // const onSubmit = ({email, password}) => console.log({email, password})
-  useEffect(() => {
-    if(submissionResult !== undefined){
-      setTimeout(() => {
-        setSubmissionResult(undefined);
-        setSubmitted(false);
-      }, submissionResult ? 1000 : 5000)
-    }
-  }, [submissionResult, setSubmissionResult, setSubmitted])
-  const onSubmit = async ({email, password}) => {
-    console.log({email, password});
-    setSubmitted(true)
-    const [result,] = await login({email, password})
-    setSubmissionResult(result);
-  }
+  const { onClick, render } = useMakeLoadingButton({buttonText: "Sign In", doAction: login})
   return (
     <Dialog open={open} onClose={() => null}
       disableEscapeKeyDown onBackdropClick={()=>null} 
     >
       <DialogTitle>Sign In or Create An Account</DialogTitle>
       <DialogContent>
-        <DialogContentText sx={{pb: 2}}>You need to sign in to use the Freeskater Finder.</DialogContentText>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <DialogContentText sx={{pb: 2}}>You need to have an account to use the Freeskater Finder.</DialogContentText>
+        <form onSubmit={handleSubmit(onClick)}>
           <Grid container direction="column">
             <TextField label="Email" margin="none" inputProps={
               register("email", {
@@ -47,7 +32,7 @@ export function SignInDialog({open}) {
               }), type: "password"}}
               error={!!errors.password} helperText={errors?.password?.message}
             />
-            <LoadingButton type="submit" margin="normal" loading={submitted} variant="contained">Sign In</LoadingButton>
+            { render({type: "submit"}) }
             <Button component={Link} to="/create-account">Create An Account</Button>
           </Grid>
         </form>
