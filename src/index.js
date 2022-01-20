7817ce84034c007fef02280f1896680d3419f399
Jeffdude <jeffmilling@gmail.com>
2022-01-20 00:58:23 +0000

    pretty much nothing :/

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 
 import { JFHApp } from '@jeffdude/frontend-helpers';
-import { FFUserProvider } from './modules/user-context'
 
 ReactDOM.render(
   <React.StrictMode>
-    <JFHApp config={{backendURL: "http://192.168.0.88:3600"}}>
-      <FFUserProvider>
-        <App />
-      </FFUserProvider>
+    <JFHApp config={{backendURL: "http://192.168.1.8:3600"}}>
+      <App />
     </JFHApp>
   </React.StrictMode>,
   document.getElementById('root')
