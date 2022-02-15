708f06fca753a3d40f06c9c09c27dba68cbc6ce8
Jeffdude <jeffmilling@gmail.com>
2022-02-15 01:10:02 +0000

    more ambassador stuff

--- src/components/submission-list.js
+++ src/components/submission-list.js
@@ -4,7 +4,7 @@ import List from '@mui/material/List'
 import DeleteIcon from '@mui/icons-material/Delete';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import { Grid, ListItemButton, ListItemSecondaryAction, Typography, IconButton, ListItemText, ListItemIcon } from '@mui/material'
+import { Link, Grid, ListItemButton, ListItemSecondaryAction, Typography, IconButton, ListItemText, ListItemIcon, ListItem, Collapse } from '@mui/material'
 import { ISOToReadableString } from '@jeffdude/frontend-helpers';
 
 import { useDeleteSubmission } from '../hooks/challenges';
@@ -17,32 +17,77 @@ function StatusIndicator({status}) {
     "DENIED": "#D84B3A",
   }[status]
   return <Grid container direction="row" sx={{alignItems: "center"}}>
-    <Typography variant="subtitle2" sx={{mr: 2}}>Submission Status:</Typography>
+    <Typography variant="subtitle2" sx={{mr: 2}}>Status:</Typography>
     <div style={{padding: "3px 10px", borderRadius: "20px", backgroundColor}}><Typography variant="button">{status}</Typography></div>
   </Grid>
 }
 
-function SubmissionItem({submission}){
+function SubmissionItem({submission, expandable = true, onClick = () => null}){
   const deleteSubmission = useDeleteSubmission({submissionId: submission._id})
   const [expanded, setExpanded] = useState(false);
-  return <ListItemButton onClick={() => setExpanded(!expanded)}>
-    <ListItemIcon>{expanded ? <KeyboardArrowDownIcon/> : <KeyboardArrowUpIcon/>}</ListItemIcon>
-    <ListItemText
-      primary={"Submitted on " + ISOToReadableString(submission.createdAt)}
-      secondary={<StatusIndicator status={submission.status}/>}
-    />
-    <ListItemSecondaryAction>
-      <IconButton onClick={() => deleteSubmission()}>
-        <DeleteIcon />
-      </IconButton>
-    </ListItemSecondaryAction>
-  </ListItemButton>
+  const [confirmed, setConfirmed] = useState(false);
+  const handleClick = (e) => {
+    if(expandable) return setExpanded(!expanded)
+    return onClick(e)
+  }
+  return <>
+    <ListItemButton onClick={handleClick} selected={expanded} sx={{minWidth: "min(90vw, 550px)"}}>
+      {expandable && <ListItemIcon>{expanded ? <KeyboardArrowDownIcon/> : <KeyboardArrowUpIcon/>}</ListItemIcon>}
+      <ListItemText
+        primary={"Submitted on " + ISOToReadableString(submission.createdAt)}
+        secondary={<StatusIndicator status={submission.status}/>}
+      />
+      {expandable 
+        ? <ListItemSecondaryAction>
+            <IconButton onClick={() => {
+              if(confirmed) {
+                deleteSubmission()
+              } else { setConfirmed(true) }
+            }}>
+              <DeleteIcon />
+              {confirmed && <Typography variant="button">?</Typography>}
+            </IconButton>
+          </ListItemSecondaryAction>
+        : <Link>See More</Link>
+      }
+    </ListItemButton>
+    <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <List component="div" disablePadding sx={{pl: 2}}>
+        {submission.content.map(({fieldObj : field, content}, index) => (
+          <ListItem key={index} dense sx={{...index % 2 ? {backgroundColor: "#dfdfdf"} : {} }} >
+            <Grid container direction="row" sx={{width: "100%", justifyContent: "space-between"}}>
+              <Typography variant="subtitle1"><b>{field.title}</b></Typography>
+              <Typography variant="subtitle1" sx={{ml: 2}}>{
+                field.fieldType !== 'YES_NO'
+                  ? content
+                  : content
+                  ? 'Yes'
+                  : 'No'
+              }</Typography>
+            </Grid>
+          </ListItem>
+        ))}
+      </List>
+    </Collapse>
+  </>
 }
 
-function SubmissionList({challenge}) {
-  return <PageCard header={<Typography variant="h6">My {challenge.title} Submissions</Typography>}>
-    <List sx={{minWidth: "min(90vw, 400px)"}}>
-      {challenge.submissions.map((submission, index) => <SubmissionItem submission={submission} key={index}/>)}
+function SubmissionList({challenge, ...props}) {
+  const challengeFields = {}
+  challenge.structure.forEach(({_id, ...field}) => challengeFields[_id] = field);
+
+  return <PageCard header={<Typography variant="h6">My {challenge.title} Submission</Typography>}>
+    <List>
+      {challenge.submissions.map(
+        submission => {
+          submission.content.forEach( // janky frontend lookups because mongodb embedded schemas are weird :(
+            (field, index) => submission.content[index].fieldObj = challengeFields[submission.content[index].field]
+          )
+          return submission;
+        }
+      ).map((submission, index) => (
+        <SubmissionItem challengeFields={challengeFields} submission={submission} key={index} {...props}/>
+      ))}
     </List>
   </PageCard>
 } 
