9e8a8bf934b8dc3fa4b703c5ba0effc23681c803
Jeffdude <jeffmilling@gmail.com>
2022-03-17 21:44:21 +0000

    add month picker field

--- src/components/forms/edit-profile.js
+++ src/components/forms/edit-profile.js
@@ -5,7 +5,7 @@ import { useGetUserInfo, usePatchUser } from '@jeffdude/frontend-helpers';
 import { Button } from '@mui/material';
 
 import useMakeForm from '../../hooks/form'
-import { makeTextField } from './fields'
+import { makeTextField, makeMonthPickerField } from './fields'
 import PageCard from '../page-card';
 import UserAvatar from '../user-avatar';
 
@@ -15,25 +15,36 @@ function EditProfileCard({onSuccess = () => null, title, noProfileIcon = false, 
   const patchUser = usePatchUser();
 
   const maxBioLength = 200;
+  const today = new Date();
+
+  const makeTextFieldState = ([key, label]) => ({
+    key, label, initialState: userInfo[key],
+    component: makeTextField({key, label, validation: {
+      required: 'This field is required.',
+      ...(key === "bio" 
+        ? {validate: value => value.length < maxBioLength || 'Max Bio Length Exceeded (' + value.length + ')'} 
+        : {}
+      ),
+    }}),
+    formatFn: i => i,
+  })
 
   const renderForm = useMakeForm({
     actionFn: patchUser,
     onSuccess: (result) => {if(result) onSuccess(result)},
     stateList: [
-      ["firstName" , "First Name"],
-      ["lastName", "Last Name"],
-      ["bio", "User Bio (" + maxBioLength + " characters max)"],
-    ].map(([key, label]) => ({
-      key, label, initialState: userInfo[key],
-      component: makeTextField({key, label, validation: {
-        required: 'This field is required.',
-        ...(key === "bio" 
-          ? {validate: value => value.length < maxBioLength || 'Max Bio Length Exceeded (' + value.length + ')'} 
-          : {}
-        ),
-      }}),
-      formatFn: i => i,
-    })),
+      makeTextFieldState(["firstName" , "First Name"]),
+      makeTextFieldState(["lastName", "Last Name"]),
+      makeTextFieldState(["bio", "User Bio (" + maxBioLength + " characters max)"]),
+      {
+        key: "skaterSince", label: "When did you first start freeskating?", initialState: userInfo.startedSkating,
+        formatFn: i => i.toISOString(),
+        component: makeMonthPickerField({
+          key: "skaterSince", label: "When did you first start freeskating?",
+          validation: {validate: (value => value < today || "Invalid Date.")},
+        })
+      },
+    ],
     backButton: (backButton 
       ? () => <Button component={Link} to="/my-account">Cancel</Button>
       : () => null
