8caca7fbabd051d14072efc3c641def0917a1be1
Jeffdude <jeffmilling@gmail.com>
2022-03-18 23:28:18 +0000

    formatting + code sharing

--- src/components/request-list.js
+++ src/components/request-list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Grid, ListItemAvatar, ListItemText, ListItem, List, Link as MuiLink, ButtonGroup } from '@mui/material';
+import { Grid, ListItemAvatar, ListItemText, ListItem, List, Link as MuiLink, ButtonGroup, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import CancelIcon from '@mui/icons-material/Cancel';
@@ -9,16 +9,11 @@ import { invalidateJFHCache } from '@jeffdude/frontend-helpers';
 import UserAvatar from '../components/user-avatar';
 import { useAcceptFriendRequest, useIgnoreFriendRequest } from '../hooks/friends';
 import useMakeLoadingButton from '../hooks/loading-button';
-import { maxBlurbLength } from '../constants';
+import { default as UserItem, UserBio } from './user-item';
 
 
 function RequestItem({request, divider}){
   const { from: user } = request;
-
-  let blurb = user.bio ? user.bio.substring(0, maxBlurbLength) : ''
-  if(blurb.length > maxBlurbLength) {
-    blurb += "..."
-  }
 
   const acceptRequest = useAcceptFriendRequest(request._id);
   const ignoreRequest = useIgnoreFriendRequest(request._id);
@@ -39,27 +34,20 @@ function RequestItem({request, divider}){
   })
 
   return (
-    <ListItem divider={divider}>
-      <ListItemAvatar>
-        <UserAvatar user={user}/>
-      </ListItemAvatar>
-      <ListItemText
-        primary={<><b>{user.firstName + " " + user.lastName}</b>{" - \"" + request.memo + "\""}</>}
-        secondary={blurb}
-        sx={{maxWidth: '80vw', flexGrow: 1}}
-      />
-      <MuiLink component={Link} to={"/location/" + user.location._id} sx={{ml: 2, flexGrow: 1}}>{user.location.zip}, {user.location.country}</MuiLink>
-      <>
+    <UserItem user={user} showLocation showAction={false} divider={divider}
+      action={<>
         <ButtonGroup variant="outline" sx={{ml: 2, display: {xs: 'none', md: 'flex'}}}> 
           {renderAcceptButton({onClick: onClickAccept, sx: {minWidth: undefined, maxWidth: undefined}, variant: "outlined"})}
           {renderIgnoreButton({onClick: onClickIgnore, sx: {minWidth: undefined, maxWidth: undefined}, variant: "outlined"})}
         </ButtonGroup> 
-        <Grid container direction="row" sx={{maxWidth: '25vw', "&>*": {xs: 'auto'}, display: {xs: 'flex', md: 'none'}}}> 
+        <Grid container direction="column" sx={{maxWidth: '25vw', justifyContent: 'flex-end', display: {xs: 'flex', md: 'none'}}}> 
           {renderAcceptButton({onClick: onClickAccept, sx: {minWidth: undefined, maxWidth: undefined}, variant: "outlined"})}
           {renderIgnoreButton({onClick: onClickIgnore, sx: {minWidth: undefined, maxWidth: undefined}, variant: "outlined"})}
         </Grid> 
-      </>
-    </ListItem>
+      </>}
+    >
+      <Typography variant="body1" sx={{mt: 2}}>- <i>"{request.memo}"</i></Typography>
+    </UserItem>
   )
 }
 
