063f9988bc7d0d018bf204527a07c720680be38b
Jeffdude <jeffmilling@gmail.com>
2022-03-22 16:06:11 +0000

    remove unused packages

--- src/hooks/loading-button.js
+++ src/hooks/loading-button.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import 'regenerator-runtime/runtime'
 
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import ErrorIcon from '@mui/icons-material/Error';
