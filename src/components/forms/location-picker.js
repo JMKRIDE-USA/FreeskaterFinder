dce483db7e4033fea6ab9b5ab05ebbe578675e35
Jeffdude <jeffmilling@gmail.com>
2022-04-28 17:43:17 +0000

    Add Help for location CU-2ufhchx

--- src/components/forms/location-picker.js
+++ src/components/forms/location-picker.js
@@ -1,9 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useReducer } from 'react';
 
-import { Grid, TextField, Typography } from '@mui/material';
+import { Grid, TextField, Typography, Link, Dialog, DialogTitle, List, ListItem, ListItemText, ListItemAvatar } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import { Circle } from '@react-google-maps/api';
 import ErrorIcon from '@mui/icons-material/Error';
+import HelpIcon from '@mui/icons-material/Help';
 
 import { useGetUserInfo } from '@jeffdude/frontend-helpers';
 
@@ -38,6 +39,7 @@ const LocationPickerCard = ({onSuccess = () => null}) => {
 
   const [location, setLocation] = useState(userInfo?.location ? userInfo.location : undefined);
   const [ error, setError ] = useState('');
+  const [ showHelp, toggleHelp ] = useReducer(state => !state, false)
 
   const lookupLocation = useLookupLocation();
   const saveLocation = useSaveLocation();
@@ -91,14 +93,40 @@ const LocationPickerCard = ({onSuccess = () => null}) => {
         <Grid container direction="row" sx={{mb: 1, justifyContent: "center"}}>
           <ErrorIcon sx={{color: "red", mr: 1}}/>
           <Typography variant="button" sx={{color: "red"}}>{error}</Typography>
+          <Link sx={{ml: 3}} onClick={toggleHelp}>Get Help</Link>
         </Grid>
       }
+      <Dialog onClose={toggleHelp} open={showHelp}>
+        <DialogTitle>Location Help</DialogTitle>
+        <List>
+          <ListItem>
+            <ListItemAvatar><ErrorIcon/></ListItemAvatar>
+            <ListItemText primary={"Error: No Results Found"} secondary={
+              <>Please search for the location on <Link href="https://google.com/maps" target="_blank">Google Maps</Link> and use the postal code that it provides.</>
+            }/>
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar><ErrorIcon/></ListItemAvatar>
+            <ListItemText primary={"Error: Defined Area Is Too Specific"} secondary={
+              "The found area is too small, which is dangerous for the security of our users. " +
+              "Postal codes in some countries, like Canada, have several layers of location, e.g. \'SK S4T 3N2\', which can be made less specific by removing parts of the code, like \'SK S4T\'."
+            }/>
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar><HelpIcon/></ListItemAvatar>
+            <ListItemText primary={"Contact Me: jeff@jmkride.com"} secondary={
+              <>If neither of the above are working, please send me an email containing your desired location and a link to the pin on <Link href="https://google.com/maps" target="_blank">Google Maps</Link>.
+              I will work with you to fix the issue. Sorry for any inconvenience</>
+            }/>
+          </ListItem>
+        </List>
+      </Dialog>
       {location && <>
         <Grid container direction="row" sx={{justifyContent: "center", alignItems: "center"}}>
           <Typography variant="body1">Does this look correct?</Typography>
           {renderSaveButton({sx: {m: 1}})}
         </Grid>
-        <Typography variant="subtitle2">"{location?.formatted_address}"</Typography>
+        {location?.formatted_address && <Typography variant="subtitle2">"{location?.formatted_address}"</Typography>}
       </>}
       <Map center={location ? location : undefined} zoom={location ? 9 : undefined}>
         {location && <ResultCircle location={location}/>}
